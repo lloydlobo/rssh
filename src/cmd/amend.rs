9a3bcebeaadb77a509972d087e90b662bce61014
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-04 10:28:08 +0530

    feat: implement amend  module run function

--- src/cmd/amend.rs
+++ src/cmd/amend.rs
@@ -2,6 +2,26 @@ use xshell::{cmd, Shell};
 
 pub fn run(sh: &Shell) -> anyhow::Result<()> {
     cmd!(sh, "git add . ").run()?;
+    cmd!(sh, "git --no-pager diff --cached --color=always").run()?;
+    cmd!(sh, "git --no-pager show -s --oneline --abbrev-commit --color=always HEAD").run()?;
+
+    if is_yes_or_no_prompt("Continue") {
+        cmd!(sh, "git commit --amend --no-edit").run()?;
+    }
 
     Ok(())
+}
+
+/// Returns a `bool` indicating if the user decided to continue when prompted.
+/// Locks this handle and reads a line of input, appending it to the specified buffer.
+fn is_yes_or_no_prompt(arg_msg: &str) -> bool {
+    println!("{arg_msg}");
+    let mut buf = String::new();
+    match std::io::stdin().read_line(&mut buf) {
+        Err(_) | Ok(0) => false,
+        Ok(_) => {
+            let response: &str = buf.trim();
+            matches!(response, "" | "y" | "Y")
+        }
+    }
 }
