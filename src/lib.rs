82a67c05695171eb3291c05da84ca92396142557
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-04 10:08:48 +0530

    chore: move tests to lib module

--- src/lib.rs
+++ src/lib.rs
@@ -68,3 +68,52 @@ pub fn walkdir(path: PathBuf) -> anyhow::Result<Vec<PathBuf>> {
 
     Ok(res)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use anyhow::Context;
+    use xshell::{cmd, Shell};
+
+    use super::*;
+
+    #[test]
+    fn link_up() {
+        let sh = Shell::new().unwrap();
+        let bin = std::path::Path::new("/home/lloyd/bin");
+        cmd!(sh, "cargo build --release").run().unwrap();
+
+        for &(cmd, _) in COMMANDS {
+            let dst: PathBuf = bin.join(cmd);
+            sh.remove_path(&dst).unwrap();
+            let _ = cmd!(sh, "git rm {dst} -f").ignore_stderr().quiet().run();
+            sh.hard_link("./target/release/rssh", &dst).unwrap();
+        }
+
+        let home: PathBuf = "/home/lloyd/".into();
+        let config_home: PathBuf = home.join("config/home");
+        for abs_path in walkdir(config_home.clone()).unwrap() {
+            let rel_path: &Path = abs_path.strip_prefix(&config_home).unwrap();
+            let dest: PathBuf = home.join(rel_path);
+            sh.remove_path(&dest).unwrap(); // Removes the file or directory at the given path.
+            sh.create_dir(dest.parent().unwrap()).unwrap(); // Creates the specified directory.
+
+            // The `link` path will be a symbolic link pointing to the `original` path.
+            std::os::unix::fs::symlink(abs_path, dest).unwrap();
+        }
+    }
+
+    #[test]
+    fn it_rssh_file_stem() {
+        assert_eq!(
+            "rssh",
+            Path::new("rssh.rs")
+                .file_stem()
+                .context(anyhow!(
+                    "Should extract the stem (non-extension) portion of [`self.file_name`]."
+                ))
+                .unwrap()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,6 @@ use anyhow::{anyhow, Context};
 use rssh::{build_link, COMMANDS};
 use xshell::Shell;
 
-// NOTE:
-// Looks like linking build_link works when in the projects directory.
-// * fixed: `let src = &"./target/release/rssh";`
-// * with: `let src = &"/home/lloyd/.cargo/bin/rssh";`
-
 fn main() -> anyhow::Result<()> {
     if let true = env::args().into_iter().find(|a| a == "rssh").is_some() {
         build_link().context(anyhow!("Called `rssh` to symlink COMMANDS with rssh binary"))?;
@@ -29,53 +24,4 @@ fn main() -> anyhow::Result<()> {
 
     let sh = Shell::new()?;
     run(&sh).map_err(|err| anyhow!("Failed to run {}: {:#?}", cmd_name, err))
-}
-
-#[cfg(test)]
-mod tests {
-    use std::path::Path;
-
-    use rssh::{walkdir, COMMANDS};
-    use xshell::{cmd, Shell};
-
-    use super::*;
-
-    #[test]
-    fn link_up() {
-        let sh = Shell::new().unwrap();
-        let bin = std::path::Path::new("/home/lloyd/bin");
-        cmd!(sh, "cargo build --release").run().unwrap();
-
-        for &(cmd, _) in COMMANDS {
-            let dst: PathBuf = bin.join(cmd);
-            sh.remove_path(&dst).unwrap();
-            let _ = cmd!(sh, "git rm {dst} -f").ignore_stderr().quiet().run();
-            sh.hard_link("./target/release/rssh", &dst).unwrap();
-        }
-
-        let home: PathBuf = "/home/lloyd/".into();
-        let config_home: PathBuf = home.join("config/home");
-        for abs_path in walkdir(config_home.clone()).unwrap() {
-            let rel_path: &Path = abs_path.strip_prefix(&config_home).unwrap();
-            let dest: PathBuf = home.join(rel_path);
-            sh.remove_path(&dest).unwrap(); // Removes the file or directory at the given path.
-            sh.create_dir(dest.parent().unwrap()).unwrap(); // Creates the specified directory.
-
-            // The `link` path will be a symbolic link pointing to the `original` path.
-            std::os::unix::fs::symlink(abs_path, dest).unwrap();
-        }
-    }
-
-    #[test]
-    fn it_rssh_file_stem() {
-        assert_eq!(
-            "rssh",
-            Path::new("rssh.rs")
-                .file_stem()
-                .context(anyhow!(
-                    "Should extract the stem (non-extension) portion of [`self.file_name`]."
-                ))
-                .unwrap()
-        );
-    }
 }
