74aebd6a3ef7dded098e0ccbdfc91ae2c09a6eac
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-10 11:24:17 +0530

    docs: update cron docs

--- src/cmd/cron.rs
+++ src/cmd/cron.rs
@@ -1,8 +1,9 @@
-//! The run function is hardcoded to only run the rustup command at specific times as defined by the
-//! expression string.
-//!
-//! To allow for more flexible scheduling, you may want to consider allowing the user to provide a
-//! cron-style expression as a command line argument.
+//! The code is a Rust program that runs a given shell utility using cron syntax to schedule the
+//! execution. The program parses the command line arguments to get the utility to run and the cron
+//! expression (if provided), and then creates a Schedule value from the expression string. If the
+//! expression string is not provided, a default value is used, which runs the command every 10
+//! seconds. If the expression string is invalid, the program prints a default values chart and an
+//! ASCII art illustration of the cron syntax, and returns an error message.!
 //!
 //! ```yml
 //! "0 0 * * * *" - Run every day at midnight (i.e. 0 hours, 0 minutes, 0 seconds).
@@ -15,9 +16,7 @@
 //! "0 0 12 * * *" - Run every day at noon (i.e. 12 hours, 0 minutes, 0 seconds).
 //! "0 0 0 * * 6" - Run every Saturday at midnight (i.e. 0 hours, 0 minutes, 0 seconds).
 //! ```
-//!
-//! These are just a few examples, and you can adjust the values based on your specific needs. The
-//! format for cron syntax is as follows:
+//! The format for cron syntax is as follows:
 //!
 //! ```txt
 //! * * * * * *
@@ -30,24 +29,21 @@
 //! ------------- Second (0 - 59)
 //! ```
 
-use std::{fmt::Write, time::Duration};
+use std::time::Duration;
 
 use anyhow::{anyhow, Result};
 use cron::Schedule;
 use job_scheduler_ng::{Job, JobScheduler};
 use xshell::{cmd, Shell};
 
-// use std::{str::FromStr, sync::RwLock};
-// use chrono::Utc; use cron::Schedule;
+const DEFAULT_EXPRESSION: &str = "1/10 * * * * *";
 
-/// The `run` function takes a reference to a `Shell` instance and returns a `Result`.
 /// It uses the `xflags` crate to parse the command line arguments, specifically the required
 /// `utility` argument and the optional `expression` argument.
 ///
 /// If the `expression` argument is not provided, the default value of "1/10 * * * * *" is used,
 /// which means the command will get executed every 10 seconds. If the `utility` argument is
 /// `"fortune"`, the `run_fortune_command` function is called with the parsed `expression`.
-///
 /// If the `utility` argument is unknown, an error message is returned.
 ///
 /// # Usage
@@ -64,6 +60,9 @@ use xshell::{cmd, Shell};
 /// $ fortune
 /// Sorry.  I forget what I was going to say.
 /// ```
+///
+/// To allow for more flexible scheduling, you may want to consider allowing the user to provide a
+/// cron-style expression as a command line argument.
 pub fn run(_sh: &Shell) -> Result<()> {
     // Parse the command-line arguments using xflags.
     let flags = xflags::parse_or_exit! {
@@ -71,25 +70,20 @@ pub fn run(_sh: &Shell) -> Result<()> {
         optional expression: String    // Cron expresion.
     };
 
-    // Parse the `expression` string into a `Schedule` value.
-    const DEFAULT_EXP: &str = "1/10 * * * * *";
-    let expression: &str = flags.expression.as_deref().unwrap_or(DEFAULT_EXP); // default value
-    let expression: Schedule = match expression.parse::<Schedule>() {
+    let expression: &str = flags.expression.as_deref().unwrap_or(DEFAULT_EXPRESSION);
+
+    let schedule: Schedule = match expression.parse::<Schedule>() {
+        Ok(sched) => sched,
         Err(err) => {
-            eprintln!(
-                "{}\n{}\n{expression}",
-                print_cron_default_values(),
-                generate_cron_ascii_art(),
-            );
+            let available = print_cron_default_values();
+            let help = generate_cron_ascii_art();
+            eprintln!("{available}\n{help}\n{expression}");
             return Err(anyhow!(r#"unknown cron syntax: `{err}`"#,));
         }
-        Ok(it) => it,
     };
 
-    // Check if the `utility` string matches the expected value, and run the corresponding command.
-    // If the `utility` string is unknown, return an error.
     match flags.utility.as_str() {
-        "fortune" => run_fortune_cmd(expression, false)?,
+        "fortune" => run_fortune_cmd(schedule, false)?,
         utility => return Err(anyhow!("unknown utility: `{utility}`")),
     }
 
@@ -124,36 +118,6 @@ fn run_fortune_cmd(expression: Schedule, signal: bool) -> Result<()> {
     Ok(())
 }
 
-const CRON_DEFAULTS: [(&str, &str); 6] = [
-    ("Minutes", "0-59"),
-    ("Hours", "0-23"),
-    ("Day of the Month", "1-31"),
-    ("Month", "1-12 (or names)"),
-    ("Day of the Week", "0-7 (0 or 7 is Sunday, or use names)"),
-    ("Command", "Any command to be executed"),
-];
-
-/// This code generates a table with two columns, one for the field name and one for its default
-/// values, separated by a fixed width space. The maximum width of the field names is calculated to
-/// determine the width of the first column, so that both columns are aligned. The values of the
-/// CRON_DEFAULTS array are used to populate the rows of the table, and the write! macro is used to
-/// format each row.
-fn create_table() -> String {
-    let mut table = String::new();
-
-    let column_width = CRON_DEFAULTS.iter().map(|(title, _)| title.len()).max().unwrap();
-
-    for (title, default) in CRON_DEFAULTS.iter() {
-        writeln!(
-            table,
-            "{title:<column_width$}  {default}"
-        )
-        .unwrap();
-    }
-
-    table
-}
-
 /// The format is a crontab expression, which is a string that defines the schedule for a Unix-style
 /// cron job. Each field in a crontab expression corresponds to a different aspect of the schedule,
 /// such as the minute, hour, day of the month, etc. The fields are separated by spaces, and each
@@ -173,20 +137,6 @@ fn generate_cron_ascii_art() -> String {
     cron_syntax.join("\n")
 }
 
-fn generate_cron_default_values() -> Vec<&'static str> {
-    let cron_syntax = [
-        "0 0 * * * *",
-        "0 0 0 * * *",
-        "0 0 * * 0 *",
-        "0 0 0 1 * *",
-        "0 0 0 1 1 *",
-        "0 0 9 * * *",
-        "0 0 17 * * *",
-        "0 0 12 * * *",
-        "0 0 0 * * 6",
-    ];
-    cron_syntax.to_vec()
-}
 fn print_cron_default_values() -> String {
     let cron_syntax = [
         r#""0 0 * * * *" - Run every day at midnight (i.e. 0 hours, 0 minutes, 0 seconds),"#,
@@ -205,17 +155,26 @@ fn print_cron_default_values() -> String {
 #[cfg(test)]
 mod tests {
     use std::{
+        fmt::Write,
         io::{self, BufRead},
-        sync::{
-            mpsc,
-            mpsc::TryRecvError,
-        },
+        sync::{mpsc, mpsc::TryRecvError},
         thread,
-        time::{Duration},
+        time::Duration,
     };
 
     use super::*;
 
+    // Run command every 2 seconds till `dur` elapses.
+    fn try_fortune_cmd(signal: bool) {
+        let expression = "1/2 * * * * *".parse::<Schedule>().expect("Failed to parse cron syntax");
+        assert!(run_fortune_cmd(expression, signal).is_ok());
+    }
+
+    /// Terminated externally
+    ///
+    /// On each iteration of a worker loop, we check if someone notified us through a channel. If
+    /// yes or if the other end of the channel has gone out of scope we break the loop.
+    /// https://stackoverflow.com/a/26200583
     #[test]
     fn try_terminate() {
         println!("Press enter to terminate the child thread");
@@ -240,7 +199,13 @@ mod tests {
 
         let _ = tx.send(());
     }
-
+    /// Suspending and resuming
+    ///
+    /// We use recv() which suspends the thread until something arrives on the channel. In order to
+    /// resume the thread, you need to send something through the channel; the unit value () in this
+    /// case. If the transmitting end of the channel is dropped, recv() will return Err(()) - we use
+    /// this to exit the loop.      
+    /// https://stackoverflow.com/a/26200583
     #[test]
     fn try_suspend_term_main() {
         println!("Press enter to wake up the child thread");
@@ -268,29 +233,42 @@ mod tests {
         }
     }
 
-    fn try_fortune_cmd(signal: bool) {
-        // Run command every 2 seconds till `dur` elapses.
-        let expression = "1/2 * * * * *".parse::<Schedule>().expect(
-            "Failed to parse cron
-    syntax",
-        );
-        assert!(run_fortune_cmd(expression, signal).is_ok());
+    /// This code generates a table with two columns, one for the field name and one for its default
+    /// values, separated by a fixed width space. The maximum width of the field names is calculated
+    /// to determine the width of the first column, so that both columns are aligned. The values
+    /// of the CRON_DEFAULTS array are used to populate the rows of the table, and the write!
+    /// macro is used to format each row.
+    #[allow(dead_code)]
+    fn create_table() -> String {
+        const CRON_DEFAULTS: [(&str, &str); 6] = [
+            ("Minutes", "0-59"),
+            ("Hours", "0-23"),
+            ("Day of the Month", "1-31"),
+            ("Month", "1-12 (or names)"),
+            ("Day of the Week", "0-7 (0 or 7 is Sunday, or use names)"),
+            ("Command", "Any command to be executed"),
+        ];
+        let mut table = String::new();
+        let column_width = CRON_DEFAULTS.iter().map(|(title, _)| title.len()).max().unwrap();
+        for (title, default) in CRON_DEFAULTS.iter() {
+            writeln!(table, "{title:<column_width$}  {default}").unwrap();
+        }
+        table
     }
-    //
-    // #[test]
-    // #[should_panic]
-    // fn test_run_expression_invalid() {
-    //     wait_to_exit_par_thread();
-    //     let expression = "invalid expression";
-    //     let result = run_test_thread(expression);
-    //     assert!(result.is_err());
-    // }
-    //
-    // #[test]
-    // fn test_run_fortune_cmd_with_default_cron_syntax() {
-    //     for syntax in generate_cron_default_values() {
-    //         let expression = syntax.parse::<Schedule>().expect("Failed to parse cron syntax");
-    //         assert!(run_fortune_cmd(expression).is_ok());
-    //     }
-    // }
+
+    #[allow(dead_code)]
+    fn generate_cron_default_values() -> Vec<&'static str> {
+        let cron_syntax = [
+            "0 0 * * * *",
+            "0 0 0 * * *",
+            "0 0 * * 0 *",
+            "0 0 0 1 * *",
+            "0 0 0 1 1 *",
+            "0 0 9 * * *",
+            "0 0 17 * * *",
+            "0 0 12 * * *",
+            "0 0 0 * * 6",
+        ];
+        cron_syntax.to_vec()
+    }
 }
